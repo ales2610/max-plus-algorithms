4f527abdcc8909a86d6abd5ffb5038278287b3e3
ales2610 <33655760+ales2610@users.noreply.github.com>
2025-12-19 13:54:20 +0100

    Update example_lea.m

--- example_lea.m
+++ example_lea.m
@@ -1,4 +1,4 @@
-%initialize matrix
+% Initialize matrix
 A = [-inf -inf -inf -inf -inf  74  -inf  78  -inf -inf -inf -inf -inf -inf -inf -inf
      -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf  210 -inf  192 -inf
      -inf  48  -inf  61  -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf 
@@ -16,11 +16,13 @@ A = [-inf -inf -inf -inf -inf  74  -inf  78  -inf -inf -inf -inf -inf -inf -inf 
      -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf   0  -inf -inf -inf -inf -inf
      -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf -inf   0  -inf -inf -inf -inf];
 
-%parameters of this example
+% Parameters of this example
 c = 4;
 j = 3;
 delta = 10^(-8);
 N_max = 250;
 M = 1000;
 
-[delta_min, delta_max, low_95, upp_95] = lea(A, c, j, N_max, M, delta, false)
+% Use LEA algorithm to get lower and upper bound for Lyapunov exponent. For this example both should be around 53.66345.
+[delta_min, delta_max, low_95, upp_95] = lea(A, c, j, N_max, M, delta, true)
+
